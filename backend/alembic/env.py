8eb826696d55a8b4e0aa7be64a976f8462cf0db4
Rimsha Fiaz <rimshafiaz63@gmail.com>
2026-08-05 16:51:09 +0500

    Added user model table and performed alembic migration

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -5,6 +5,7 @@ from sqlalchemy import engine_from_config, pool
 
 from app.core.config import settings
 from app.db.base import Base
+from app.models.user import User
 
 # Alembic Config object
 config = context.config
@@ -59,4 +60,3 @@ if context.is_offline_mode():
     run_migrations_offline()
 else:
     run_migrations_online()
-    
